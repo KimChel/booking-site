16efda50d72d6f4e7e6a81fa3c65b7461d3d5308
parakakakias <kimonheliotis@yahoo.gr>
2022-06-09 16:46:39 +0300

    added login

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,5 +1,6 @@
 import axios from "axios"
 import { useContext, useState } from "react"
+import { useNavigate } from "react-router-dom"
 import { AuthContext } from "../../context/AuthContext"
 import "./login.css"
 
@@ -9,7 +10,8 @@ const Login = () => {
         password: undefined,
     })
 
-    const { user, loading, error, dispatch } = useContext(AuthContext)
+    const {loading, error, dispatch } = useContext(AuthContext)
+    const navigate = useNavigate()
 
     const handleChange = (e) => {
         setCredentials((prev) => ({ ...prev, [e.target.id]: e.target.value }))
@@ -19,16 +21,13 @@ const Login = () => {
         e.preventDefault()
         dispatch({ type: "LOGIN_START" })
         try {
-            const res = await axios.post("/auht/login", credentials)
+            const res = await axios.post("/auth/login", credentials)
             dispatch({ type: "LOGIN_SUCCESS", payload: res.data })
+            navigate("/")
         } catch (err) {
             dispatch({ type: "LOGIN_FAILURE", payload: err.response.data })
         }
-
-        
-
     }
-    console.log(user)
 
     return <div className="login">
         <div className="loginContainer">
